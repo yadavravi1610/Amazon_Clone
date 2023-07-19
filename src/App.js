179b1de244df660ebab2fbe787d0eb733019a19d
Ravinder <ravinderyadav160@gmail.com>
2023-07-19 23:52:58 +0530

    Adding Home and Products js files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Slider from './Home/Slider';
+
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header';
 import HeaderBottom from './Components/Header/HeaderBottom';
@@ -9,6 +9,7 @@ import {
   Outlet,
 } from "react-router-dom";
 import { productsData } from "./api/api";
+import Home from './Home';
 
 const Layout=()=>{
   return(
@@ -22,13 +23,7 @@ const Layout=()=>{
   );
 }
 function App() {
-  const images =[
-    "https://images-eu.ssl-images-amazon.com/images/G/31/prime/PD23/ACQ/hero/v2/PC_Hero_3000x1200_2X_EN._CB600991698_.jpg",
-    "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Beauty/GW/ATF/revised/new/Skincare-Herofader-PC._CB594538667_.jpg",
-    "https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2023/BAU2023/ATFGW/July_Bedsheets_Desk_3000x1200_Unrec._CB601155232_.jpg",
-    "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Books/BB/JULY/GW/3000x1200-Read-lead--succeed-BB_July_2023._CB601575988_.jpg",
-    "https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Shreyansh/BAU/Unrexc/D70978891_INWLD_BAU_Unrec_Uber_PC_Hero_3000x1200._CB594707876_.jpg"
-  ]
+  
   const router = createBrowserRouter([
     {
       path : "/",
@@ -38,7 +33,7 @@ function App() {
         {
           index : true,
           loader: productsData,
-          element : <Slider images={images} />,
+          element : <Home />,
         },
       ]
     },
---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Slider from './Home/Slider'
+import Products from './Home/Products';
+
+const Home = () => {
+    const images = [
+        "https://images-eu.ssl-images-amazon.com/images/G/31/prime/PD23/ACQ/hero/v2/PC_Hero_3000x1200_2X_EN._CB600991698_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Beauty/GW/ATF/revised/new/Skincare-Herofader-PC._CB594538667_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2023/BAU2023/ATFGW/July_Bedsheets_Desk_3000x1200_Unrec._CB601155232_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Books/BB/JULY/GW/3000x1200-Read-lead--succeed-BB_July_2023._CB601575988_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Shreyansh/BAU/Unrexc/D70978891_INWLD_BAU_Unrec_Uber_PC_Hero_3000x1200._CB594707876_.jpg"
+    ]
+    return (
+        <div>
+            <Slider images={images} />
+            <Products />
+        </div>
+    )
+}
+
+export default Home
